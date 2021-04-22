34f2b3d04bb6de7b34df08b6cac2f7c91c65045b
Emmy Steven <hello@emmysteven.com>
2021-04-22 10:53:44 +0100

    🎉 first commit

---
+++ src/main/java/bamaton/motor/MotorApplication.java
@@ -0,0 +1,13 @@
+package bamaton.motor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bamaton/motor/MotorApplicationTests.java
@@ -0,0 +1,13 @@
+package bamaton.motor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
